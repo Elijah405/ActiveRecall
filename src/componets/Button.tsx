3739feeae733a5af3814f1974a50a01cc7d53a09
Elijah405 <elijahorozco405@tamu.edu>
2024-06-06 11:54:33 -0500

    renamed:    study-bear/.eslintrc.cjs -> .eslintrc.cjs 	renamed:    study-bear/.gitignore -> .gitignore 	renamed:    study-bear/README.md -> README.md 	renamed:    study-bear/index.html -> index.html 	renamed:    study-bear/package-lock.json -> package-lock.json 	renamed:    study-bear/package.json -> package.json 	renamed:    study-bear/public/vite.svg -> public/vite.svg 	renamed:    study-bear/src/App.css -> src/App.css 	renamed:    study-bear/src/App.tsx -> src/App.tsx 	renamed:    study-bear/src/CombinedComponets/WelcomePage.tsx -> src/CombinedComponets/WelcomePage.tsx 	renamed:    study-bear/src/Pages/MainPage.tsx -> src/Pages/MainPage.tsx 	renamed:    study-bear/src/assets/react.svg -> src/assets/react.svg 	new file:   src/componets/Alert.tsx 	new file:   src/componets/Button.tsx 	renamed:    study-bear/src/componets/CenteredButton.tsx -> src/componets/CenteredButton.tsx 	renamed:    study-bear/src/componets/CenteredText.tsx -> src/componets/CenteredText.tsx 	new file:   src/componets/ListGroup.tsx 	renamed:    study-bear/src/main.tsx -> src/main.tsx 	renamed:    study-bear/src/vite-env.d.ts -> src/vite-env.d.ts 	renamed:    study-bear/tsconfig.json -> tsconfig.json 	renamed:    study-bear/tsconfig.node.json -> tsconfig.node.json 	renamed:    study-bear/vite.config.ts -> vite.config.ts

---
+++ src/componets/Button.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  children: string;
+  onClick: () => void;
+}
+
+const Button = ({ children }: Props) => {
+  return <button className="btn btn-primary">{children}</button>;
+};
+
+export default Button;
