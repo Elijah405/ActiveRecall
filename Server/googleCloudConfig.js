276caa7b2a37cda04ef559b3fcbb3013621a127f
Elijah405 <elijahorozco405@tamu.edu>
2024-07-05 12:24:00 -0500

    deleted:    Server/googleCloudConfig.js 	deleted:    Server/package-lock.json 	deleted:    Server/package.json 	deleted:    Server/server.js

--- Server/googleCloudConfig.js
+++
@@ -1,9 +0,0 @@
-const { Storage } = require('@google-cloud/storage');
-const path = require('path');
-
-
-const storage = new Storage({
-    keyFilename : 'studybear-426913-34f2fd31e6dc.json' ,
-  });
-
-module.exports = storage;
