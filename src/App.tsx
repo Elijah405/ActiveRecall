22fecb3f54fed290241d082b9c5960a2f0b684a8
Elijah405 <elijahorozco405@tamu.edu>
2024-07-07 15:20:33 -0500

    modified:   index.html 	modified:   node_modules/.cache/gh-pages/https!github.com!Elijah405!ActiveRecall.git 	modified:   src/App.tsx 	modified:   vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import "./App.css";
 import WelcomePage from "./CombinedComponets/WelcomePage";
 import UploadPage from "./Pages/UploadPage";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PDFPage from "./Pages/PDFPage";
 import RandomizerPage from "./Pages/RandomizerPage";
 import LoginPage from "./CombinedComponets/LoginPage";
 import ContextProvider from "./componets/ContextProvider";
 
+// Set the basename based on the environment
+const basename = process.env.NODE_ENV === "production" ? "/ActiveRecall" : "/";
+
 function App() {
   return (
     <div>
       <ContextProvider>
-        <Router>
+        <Router basename={basename}>
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/welcome" element={<WelcomePage />} />
@@ -27,18 +29,3 @@ function App() {
 }
 
 export default App;
-
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="UTF-8" />
-//     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <title>StudyBear.Online</title>
-//     <script src="https://accounts.google.com/gsi/client" async defer></script>
-//   </head>
-//   <body>
-//     <div id="root"></div>
-//     <script type="module" src="/src/main.tsx"></script>
-//   </body>
-// </html>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ActiveRecall/', // Ensure this matches the repository name
+  base: process.env.NODE_ENV === 'production' ? '/ActiveRecall/' : '/',
   build: {
     outDir: 'dist',
   },
